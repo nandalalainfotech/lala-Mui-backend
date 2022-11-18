69d7653e69741f28ad9ecf15a04d5687a2643bcd
nandalalainfotech <94165593+nandalalainfotech@users.noreply.github.com>
2022-11-18 11:39:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,160 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import userRouter from './routers/useRouter.js';
+import productRouter from './routers/productRouter.js';
+import sareeRouter from './routers/sareeRouter.js';
+import womenRouter from './routers/womenRouter.js';
+import kidRouter from './routers/kidRouter.js';
+import tshirtRouter from './routers/TshirtRouter.js';
+import casualshirtRouter from './routers/CasualshirtRouter.js';
+import formalshirtRouter from './routers/FormalshirtRouter.js';
+import jacketRouter from './routers/JacketRouter.js';
+import sweaterRouter from './routers/SweaterRouter.js';
+import rainjacketRouter from './routers/RainJacketRouter.js';
+import blazerRouter from './routers/BlazerRouter.js';
+import suitRouter from './routers/SuitRouter.js';
+import orderRouter from './routers/orderRouter.js';
+import indianRouter from './routers/IndianRouter.js';
+import dhotisRouter from './routers/DhotisRouter.js';
+import nehruRouter from './routers/NehruRouter.js';
+import kurtasRouter from './routers/KurtasRouter.js';
+import sherwaniRouter from './routers/SherwaniRouter.js';
+import path from 'path';
+import uploadRouter from './routers/uploadRouter.js';
+import http from 'http';
+import { Server } from 'socket.io';
+import bodyParser from "body-parser";
+
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect('mongodb+srv://nandalala:Spartans!23@cluster0.ujwabrm.mongodb.net/laladb?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+
+    }).then(() => console.log('MongoDB connection established.'))
+    .catch((error) => console.error("MongoDB connection failed:", error.message))
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.use('/api/uploads', uploadRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/womens', womenRouter);
+app.use('/api/kids', kidRouter)
+app.use('/api/tshirts', tshirtRouter);
+app.use('/api/casualshirts', casualshirtRouter);
+app.use('/api/formalshirts', formalshirtRouter);
+app.use('/api/jackets', jacketRouter);
+app.use('/api/sweaters', sweaterRouter);
+app.use('/api/blazers', blazerRouter);
+app.use('/api/rainjackets', rainjacketRouter);
+app.use('/api/dhotiss', dhotisRouter);
+app.use('/api/indians', indianRouter);
+app.use('/api/nehrus', nehruRouter);
+app.use('/api/kurtass', kurtasRouter);
+app.use('/api/sherwanis', sherwaniRouter);
+app.use('/api/suits', suitRouter);
+app.use('/api/sarees', sareeRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+app.get('/api/config/google', (req, res) => {
+    res.send(process.env.GOOGLE_API_KEY || '');
+});
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../lala-frontend/public/index.html'))
+);
+
+app.use((err, req, res) => {
+    res.status && res.status(500).send({ message: err.message });
+});
+const port = process.env.PORT || 5000;
+
+// app.listen(5000, () => {
+const httpServer = http.Server(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+const users = [];
+
+io.on('connection', (socket) => {
+    socket.on('disconnect', () => {
+        const user = users.find((x) => x.socketId === socket.id);
+        if (user) {
+            user.online = false;
+            const admin = users.find((x) => x.isAdmin && x.online);
+            if (admin) {
+                io.to(admin.socketId).emit('updateUser', user);
+            }
+        }
+    });
+    socket.on('onLogin', (user) => {
+        const updatedUser = {
+            ...user,
+            online: true,
+            socketId: socket.id,
+            messages: [],
+        };
+        const existUser = users.find((x) => x._id === updatedUser._id);
+        if (existUser) {
+            existUser.socketId = socket.id;
+            existUser.online = true;
+        } else {
+            users.push(updatedUser);
+        }
+        const admin = users.find((x) => x.isAdmin && x.online);
+        if (admin) {
+            io.to(admin.socketId).emit('updateUser', updatedUser);
+        }
+        if (updatedUser.isAdmin) {
+            io.to(updatedUser.socketId).emit('listUsers', users);
+        }
+    });
+
+    socket.on('onUserSelected', (user) => {
+        const admin = users.find((x) => x.isAdmin && x.online);
+        if (admin) {
+            const existUser = users.find((x) => x._id === user._id);
+            io.to(admin.socketId).emit('selectUser', existUser);
+        }
+    });
+
+    socket.on('onMessage', (message) => {
+        if (message.isAdmin) {
+            const user = users.find((x) => x._id === message._id && x.online);
+            if (user) {
+                io.to(user.socketId).emit('message', message);
+                user.messages.push(message);
+            }
+        } else {
+            const admin = users.find((x) => x.isAdmin && x.online);
+            if (admin) {
+                io.to(admin.socketId).emit('message', message);
+                const user = users.find((x) => x._id === message._id && x.online);
+                user.messages.push(message);
+            } else {
+                io.to(socket.id).emit('message', {
+                    name: 'Admin',
+                    body: 'Sorry. I am not online right now',
+                });
+            }
+        }
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+});
