7a31b9910ca68c48dff0d38f9b0f5ca618796a1b
nandalalainfotech <nandalalainfotech@gmail.com>
2022-12-09 13:35:54 +0530

    Place Order Screen Isuue done by Aravinth.

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -102,6 +102,7 @@ orderRouter.post(
   '/',
   isAuth,
   expressAsyncHandler(async (req, res) => {
+    console.log("req-->", req.body);
     if (req.body.orderItems.length === 0) {
       res.status(400).send({ message: 'Cart is empty' });
     } else {
@@ -110,10 +111,10 @@ orderRouter.post(
         orderItems: req.body.orderItems,
         shippingAddress: req.body.shippingAddress,
         paymentMethod: req.body.paymentMethod,
-        itemsPrice: req.body.itemsPrice,
-        shippingPrice: req.body.shippingPrice,
-        taxPrice: req.body.taxPrice,
-        totalPrice: req.body.totalPrice,
+        // itemsPrice: req.body.itemsPrice,
+        // shippingPrice: req.body.shippingPrice,
+        // taxPrice: req.body.taxPrice,
+        // totalPrice: req.body.totalPrice,
         user: req.user._id,
       });
       const createdOrder = await order.save();
