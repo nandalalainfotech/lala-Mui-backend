47907a42b23ea170b03f7b13b97ed4d73c8d67e7
nandalalainfotech <nandalalainfotech@gmail.com>
2022-12-14 17:05:59 +0530

    Application Setting done by Aravinth.

--- Models/applicationModel.js
+++ Models/applicationModel.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 const applicationSchema = new mongoose.Schema(
   
     {
+      name: { type: String, required: true },
       fieldname: { type: String, required: true },
       originalname: { type: String, required: true },
       path: { type: String, required: false },
-      status: { type: String, required: false },
+      filename: { type: String, required: false },
       mimetype: { type: String, required: false },
       encoding: { type: String, required: false },
       flag: { type: Boolean, required: false },
