a136652cc041d320a26ce0873aadfda778213470
sivakumar.k@360.technology <sivakumar.k@360.technology>
2022-12-19 18:19:48 +0530

    uploadRouter created done by Naresh.

--- Models/imagesModel.js
+++ Models/imagesModel.js
@@ -1,17 +1,29 @@
 import mongoose from 'mongoose';
 const imageSchema = new mongoose.Schema(
-  
-    {
-      fieldname: { type: String, required: true },
-      filename: { type: String, required: true },
-      originalname: { type: String, required: true },
-      path: { type: String, required: false },
-      status: { type: String, required: false },
-      mimetype: { type: String, required: false },
-      encoding: { type: String, required: false },
-      flag: { type: Boolean, required: false },
+  {
+    fieldname: { type: String, required: true },
+    filename: { type: String, required: true },
+    originalname: { type: String, required: true },
+    images: [
+              {
+                _id:{ type: String, required: true },
+                fieldname: { type: String, required: true },
+                filename: { type: String, required: true },
+                originalname: { type: String, required: true },
+              },
+            ]
+  },
+    // {
+    //   fieldname: { type: String, required: true },
+    //   filename: { type: String, required: true },
+    //   originalname: { type: String, required: true },
+    //   path: { type: String, required: false },
+    //   status: { type: String, required: false },
+    //   mimetype: { type: String, required: false },
+    //   encoding: { type: String, required: false },
+    //   flag: { type: Boolean, required: false },
       
-    },
+    // },
     {
       timestamps: true,
     },
