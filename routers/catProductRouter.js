f05c9bd37edeeac325343d9cf111db8c6ff59754
nandalala <nandalalainfotech@gmail.com>
2023-01-02 16:24:06 +0530

    Catalog Product Screen done by Aravinth.

--- routers/catProductRouter.js
+++ routers/catProductRouter.js
@@ -8,7 +8,7 @@ const catProductRouter = express.Router();
 catProductRouter.post('/', isAuth, async (req, res) => {
 
     
-
+console.log("req", req.body);
     const brand = new CatlogProduct({
         prodname: req.body.prodname,
         user: req.user._id,
@@ -22,6 +22,16 @@ catProductRouter.post('/', isAuth, async (req, res) => {
         quantity: req.body.quantity,
         taxexcluded: req.body.taxexcluded,
         taxincluded: req.body.taxincluded,
+        qty: req.body.qty,
+        mqty: req.body.mqty,
+        SLocation: req.body.SLocation,
+        stockin: req.body.stockin,
+        stockout: req.body.stockout,
+        date: req.body.date,
+        height: req.body.height,
+        width: req.body.width,
+        depth: req.body.depth,
+        weight: req.body.weight,
     });
     const brandSaved = await brand.save();
     console.log("req===========>>>", brandSaved);
